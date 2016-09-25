bffcb490e75c712b85ea03b83c739106e0cd9d79
Fioger <fioger95@Live.com>
2016-09-25 13:08:42 -0400

    Completed Clip function

--- qip/hw1/HW_clip.cpp
+++ qip/hw1/HW_clip.cpp
@@ -11,4 +11,24 @@
 void
 HW_clip(ImagePtr I1, int t1, int t2, ImagePtr I2)
 {
+    IP_copyImageHeader(I1, I2);
+	int w = I1->width ();
+	int h = I1->height();
+	int total = w * h;
+
+    // init lookup table
+	int i, lut[MXGRAY];
+	for(i=0; i<MXGRAY; ++i){
+       if(i<t1) 				lut[i] = t1;
+       else if(i>=t1 && i<=t2)	lut[i] = i;
+	   else if(i>t2) 			lut[i] = t2;
+    } 
+    
+    // evaluate output: each input pixel indexes into lut[] to eval output
+	int type;
+	ChannelPtr<uchar> p1, p2, endd;
+	for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+		IP_getChannel(I2, ch, p2, type);
+		for(endd = p1 + total; p1<endd;) *p2++ = lut[*p1++];
+    }
 }
