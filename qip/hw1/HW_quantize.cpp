f5b24c8c5f387e75a26de55c84fc64d2b6f1e3d7
Fioger <fioger95@Live.com>
2016-09-25 21:23:31 -0400

    Completed Quantize without dithering

--- qip/hw1/HW_quantize.cpp
+++ qip/hw1/HW_quantize.cpp
@@ -9,4 +9,23 @@
 void
 HW_quantize(ImagePtr I1, int levels, bool dither, ImagePtr I2)
 {
+	IP_copyImageHeader(I1, I2);
+	int w = I1->width();
+	int h = I1->height();
+	int total = w * h;
+	int scale = MXGRAY / levels;
+	int bias = scale / 2;
+	
+	// init lookup table
+	int i, j, lut[MXGRAY];
+	for(int i=0; i<MXGRAY; ++i){
+		lut[i] = scale * (i/scale) + bias;
+	}
+	// evaluate output: each input pixel indexes into lut[] to eval output
+	int type;
+	ChannelPtr<uchar> p1, p2, endd;
+	for (int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+		IP_getChannel(I2, ch, p2, type);
+		for (endd = p1 + total; p1 < endd;) *p2++ = lut[*p1++];
+	}
 }
