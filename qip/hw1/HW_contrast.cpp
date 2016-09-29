2c2d5b8d7b924484c26eee0f5f8c4f942b50ad5b
Fioger <fioger95@Live.com>
2016-09-28 22:46:32 -0400

    Completed Contrast

--- qip/hw1/HW_contrast.cpp
+++ qip/hw1/HW_contrast.cpp
@@ -11,4 +11,23 @@
 void
 HW_contrast(ImagePtr I1, double brightness, double contrast, ImagePtr I2)
 {
+	IP_copyImageHeader(I1, I2);
+	int w = I1->width ();
+	int h = I1->height();
+	int total = w * h;
+
+	// init lookup table
+	int i, lut_val lut[MXGRAY];
+	for(i=0; i<MXGRAY; ++i){
+        lut_val = (contrast * (i - 128)) + 128 + brightness;
+        lut[i] = CLIP(lut_val, 0, 255);
+    }
+
+	// evaluate output: each input pixel indexes into lut[] to eval output
+	int type;
+	ChannelPtr<uchar> p1, p2, endd;
+	for(int ch = 0; IP_getChannel(I1, ch, p1, type); ch++) {
+		IP_getChannel(I2, ch, p2, type);
+		for(endd = p1 + total; p1<endd;) *p2++ = lut[*p1++];
+	}
 }
